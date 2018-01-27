b315bd3140fb8cbb49080d50223c3bb6a267761e
Ferrohound <ferrohound@gmail.com>
2018-01-27 17:13:22 -0500

    Particle and effect stuff

---
+++ Assets/scripts/camera/Orbit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+	
+	public Transform target;
+	public float radius = 1;
+	public float speed = 1;
+	public float heightAngle = 90;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		float y = radius * Mathf.Cos(heightAngle);
+		float x = radius * Mathf.Cos(Time.time * speed) * 
+			Mathf.Sin(heightAngle);
+		float z = radius * Mathf.Sin(Time.time * speed) * 
+			Mathf.Sin(heightAngle);
+			
+		Vector3 newPos = new Vector3(x, y, z);
+		transform.position = newPos + target.position;
+		transform.LookAt(target);
+	}
+}
