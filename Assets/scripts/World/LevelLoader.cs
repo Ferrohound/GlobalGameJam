f8077dccf37aa11a4604e5a7319344dc31c022fa
Ferrohound <ferrohound@gmail.com>
2018-01-28 03:40:00 -0500

    Ho-Hum!

---
+++ Assets/scripts/World/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+	
+	public string SceneName;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag != "Player")
+			return;
+		
+	}
+
+	IEnumerator LoadAsyncScene()
+    {
+		// The Application loads the Scene in the background at the same time as the current Scene.
+        //This is particularly good for creating loading screens. You could also load the Scene by build //number.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneName);
+		
+		while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+		
+	}
+}
---
+++ Assets/scripts/World/LevelUnloader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelUnloader : MonoBehaviour {
+
+	public string SceneName;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.tag != "Player")
+			return;
+		
+		SceneManager.UnloadSceneAsync(SceneName);
+	}
+	
+}
